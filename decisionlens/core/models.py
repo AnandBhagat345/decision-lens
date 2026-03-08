ea611e2224dc7ffdfcaf5c4d52c58aa4adb8a537
AnandBhagat345 <anandbhagat345@.com>
2026-03-08 17:05:30 +0530

    Saving risk level with decision

--- decisionlens/core/models.py
+++ decisionlens/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Decision(models.Model):
     text = models.TextField()
+    risk_level = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- decisionlens/core/views.py
+++ decisionlens/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .decision_engine import analyze_decision
-
+from .models import Decision
 
 def home(request):
 
@@ -9,6 +9,24 @@ def home(request):
     if request.method == "POST":
 
         text = request.POST.get("decision_text", "").lower()
+
+        # save decision
+        Decision.objects.create(text=text)
+
+        # analyze decision
         paths = analyze_decision(text)
+        
+        # save first option risk for pattern analysis
+        risk = paths[0]["risk"]
+
+        Decision.objects.create(
+            text=text,
+            risk_level=risk
+        )
 
     return render(request, "index.html", {"paths": paths})
+
+    
+def history(request):
+    decisions = Decision.objects.all().order_by('-created_at')
+    return render(request, "history.html", {"decisions": decisions})
